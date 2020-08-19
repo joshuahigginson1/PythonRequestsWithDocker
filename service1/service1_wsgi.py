186b35d0d66f06ac59e5865debddef14b204320e
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-19 10:10:05 +0100

    A basic file structure. Switching to use of feature branch model.

---
+++ service1/service1_wsgi.py
@@ -0,0 +1,14 @@
+"""A file in our app's root directory that serves as it's 'entry point'."""
+
+# Imports --------------------------------------------------------------
+
+
+
+
+
+
+
+# Run our App ----------------------------------------------------------
+
+if __name__ == "__main__":
+    service1.run(host='0.0.0.0')
---
+++ service2/service2_wsgi.py
@@ -0,0 +1,14 @@
+"""A file in our app's root directory that serves as it's 'entry point'."""
+
+# Imports --------------------------------------------------------------
+
+
+
+
+
+
+
+# Run our App ----------------------------------------------------------
+
+if __name__ == "__main__":
+    service2.run(host='0.0.0.0')
